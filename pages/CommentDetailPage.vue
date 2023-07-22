b17dd0f7ba5d6419c359a96e1937e2a8e578aba9
elli-ma <elli9225@gmail.com>
2023-07-22 18:50:17 +0800

    project creation

---
+++ pages/CommentDetailPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container mx-auto py-10">
+    <h2 class="text-center text-4xl p-5 text-blue-600 font-bold">
+      {{ comment.name }}
+    </h2>
+    <div class="bg-white p-8 rounded-lg shadow-lg">
+      <ul class="space-y-4">
+        <li class="px-4 py-2 rounded-md">
+          <b>postId:</b> {{ comment.postId }}
+        </li>
+        <li class="px-4 py-2 rounded-md"><b>id:</b> {{ comment.id }}</li>
+        <li class="px-4 py-2 rounded-md"><b>e-mail:</b> {{ comment.email }}</li>
+        <li class="px-4 py-2 rounded-md"><b>body:</b> {{ comment.body }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  async asyncData({ params, $http }) {
+    const comment = await $http.$get(
+      `https://jsonplaceholder.typicode.com/comments/${params.id}`
+    );
+    return { comment };
+  },
+};
+</script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,86 @@
+
+<template>
+  <div class="container mx-auto">
+    <h2 class="text-center text-4xl p-5 ">Comments</h2>
+    <table class="table-auto w-full bg-white shadow-lg rounded-lg overflow-hidden">
+      <thead>
+        <tr>
+          <th class="p-4 bg-blue-600 text-white text-left">id</th>
+          <th class="p-4 bg-blue-600 text-white text-left">name</th>
+          <th class="p-4 bg-blue-600 text-white text-left">e-mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="comment in paginatedComments"
+          :key="comment.id"
+          @click="goToCommentDetail(comment.id)"
+          class="cursor-pointer hover:bg-gray-100"
+        >
+          <td class="p-4">{{ comment.id }}</td>
+          <td class="p-4">{{ comment.name }}</td>
+          <td class="p-4">{{ comment.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="flex justify-center mt-5">
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md mr-3 disabled:opacity-50"
+      >
+        Back
+      </button>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md disabled:opacity-50"
+      >
+        Next
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comments: [],
+      itemsPerPage: 10,
+      currentPage: 1,
+    };
+  },
+  async fetch() {
+    const response = await this.$http.$get(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+    this.comments = response;
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.comments.length / this.itemsPerPage);
+    },
+    paginatedComments() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.comments.slice(startIndex, endIndex);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    goToCommentDetail(commentId) {
+      this.$router.push({ name: "comment-detail", params: { id: commentId } });
+    },
+  },
+};
+</script>
